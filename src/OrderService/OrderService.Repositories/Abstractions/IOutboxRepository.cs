efaa3a5e338a897ad4b9c6450b72d9da7b0b3d33
Maksim Karpenko <96451040+MnzCrsh@users.noreply.github.com>
2025-01-29 23:46:06 +0300

    add transactional behavior (#3)

--- src/OrderService/OrderService.Repositories/Abstractions/IOutboxRepository.cs
+++ src/OrderService/OrderService.Repositories/Abstractions/IOutboxRepository.cs
@@ -1,13 +1,14 @@
+using System.Data;
 using OrderService.Entities.Models;
 using OrderService.Entities.Models.Responses;
 
-namespace OrderService.Repositories;
+namespace OrderService.Repositories.Abstractions;
 
 public interface IOutboxRepository
 {
-    Task<OutboxResponseModel> SaveAsync(OutboxMessage outboxMessage);
+    Task<OutboxResponseModel> SaveAsync(OutboxMessage outboxMessage, IDbConnection connection, IDbTransaction transaction);
     
-    Task<OutboxResponseModel> UpdateAsync(OutboxMessage outboxMessage);
+    Task<bool> UpdateAsync(OutboxMessage outboxMessage, IDbConnection connection, IDbTransaction transaction);
     
-    Task<IEnumerable<OutboxResponseModel>?> FetchUnprocessedMessagesAsync();    
+    Task<IEnumerable<OutboxResponseModel>?> FetchUnprocessedMessagesAsync(IDbConnection connection, IDbTransaction transaction);    
 }
