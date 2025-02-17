7247667d597aceed591cbb2c326351cffd001022
MnzCrsh <96451040+MnzCrsh@users.noreply.github.com>
2025-02-17 21:41:04 +0300

    init

--- src/OrderService/OrderService.Fixture/seed_data.sql
+++ src/OrderService/OrderService.Fixture/seed_data.sql
@@ -6,6 +6,6 @@ VALUES  ('a1e8e9b8-1c2f-4d66-9d3f-9e4f2b7e1a11', 'UserCreated', '{"userId": 1, "
         ('e5fcabb0-5f60-8faa-df71-1f807fb05e55', 'OrderCancelled', '{"orderId": 102, "reason": "out of stock"}', '2025-02-12T16:04:00+00', NULL, 1, 0),
         ('f6fdbcc1-6f71-9fbb-ef82-2f918fc16f66', 'InventoryUpdated', '{"itemId": 55, "quantity": 150}', '2025-02-12T16:05:00+00', NULL, 1, 0),
         ('07a1bcd2-7f82-afcc-f093-3fa29fd27f77', 'ProductAdded', '{"productId": 300, "name": "Widget"}', '2025-02-12T16:06:00+00', NULL, 1, 0),
-        ('18b2cde3-8f93-b0dd-0a14-4fb3a0e38a88', 'DiscountApplied', '{"orderId": 103, "discount": 10}', '2025-02-12T16:07:00+00', NULL, 1, 0),
+        ('18b2cde3-8f93-b0dd-0a14-4fb3a0e38a88', 'DiscountNotApplied', '{"orderId": 103, "discount": 10}', '2025-02-12T16:07:00+00', NULL, 1, 0),
         ('29c3def4-9fa4-c1ee-1b25-5fc4b1f49b99', 'ShippingScheduled', '{"orderId": 104, "date": "2025-02-14"}', '2025-02-12T16:08:00+00', NULL, 1, 0),
         ('3ad4ef05-afb5-d2ff-2c36-6cd5c203ac00', 'FeedbackReceived', '{"userId": 3, "rating": 5}', '2025-02-12T16:09:00+00', NULL, 1, 0);
